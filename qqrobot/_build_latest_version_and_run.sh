a45309d522654c878b8e5ad4f48f246da1060308
风之凌殇 <fzls.zju@gmail.com>
2021-11-22 15:19:35 +0800

    revert: 不用分开

--- qqrobot/_build_latest_version_and_run.sh
+++ qqrobot/_build_latest_version_and_run.sh
@@ -4,9 +4,7 @@ set -x
 git pull --recurse-submodules
 
 # 从私有仓库子模块复制最新配置到项目根目录
-cp qqrobot/setting/config.toml .
-cp qqrobot/setting/config.yml .
-cp qqrobot/setting/device.json .
+cp qqrobot/setting/{config.toml,config.yml,device.json} .
 
 # 构建新版本
 go build -v -o qq_robot .
