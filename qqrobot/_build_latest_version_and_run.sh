d35bb1b07585f747723f98a99c21e1f5fbd1cb93
风之凌殇 <fzls.zju@gmail.com>
2021-11-22 15:14:48 +0800

    feat: 调整脚本

--- qqrobot/_build_latest_version_and_run.sh
+++ qqrobot/_build_latest_version_and_run.sh
@@ -1,14 +1,13 @@
 set -x
 
-# 拉取代码
-git pull
+# 拉取代码，包括子模块
+git pull --recurse-submodules
+
+# 从私有仓库子模块复制最新配置到项目根目录
+cp qqrobot/setting/{config.toml, config.yml, device.json} .
 
 # 构建新版本
 go build -v -o qq_robot .
-
-# 从私有仓库子模块拉取最新配置，并复制到项目根目录
-git submodule update --recursive
-cp qqrobot/setting/{config.toml, config.yml, device.json} .
 
 # 运行
 ./qq_robot faststart
