a870607c9c7c37d2f83e90002a5ce8aa75d93708
风之凌殇 <fzls.zju@gmail.com>
2021-11-22 15:03:35 +0800

    feat: 通过子模块来更新配置，从而不必手动拖入

--- qqrobot/_build_latest_version_and_run.sh
+++ qqrobot/_build_latest_version_and_run.sh
@@ -6,5 +6,9 @@ git pull
 # 构建新版本
 go build -v -o qq_robot .
 
+# 从私有仓库子模块拉取最新配置，并复制到项目根目录
+git submodule update --recursive
+cp qqrobot/setting/{config.toml, config.yml, device.json} .
+
 # 运行
 ./qq_robot faststart
