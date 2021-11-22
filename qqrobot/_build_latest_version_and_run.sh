2d4ffe982003e8d66f3eb3714c53d5f098b26043
风之凌殇 <fzls.zju@gmail.com>
2021-11-22 15:16:16 +0800

    fix: 不应该有空格

--- qqrobot/_build_latest_version_and_run.sh
+++ qqrobot/_build_latest_version_and_run.sh
@@ -4,7 +4,7 @@ set -x
 git pull --recurse-submodules
 
 # 从私有仓库子模块复制最新配置到项目根目录
-cp qqrobot/setting/{config.toml, config.yml, device.json} .
+cp qqrobot/setting/{config.toml,config.yml,device.json} .
 
 # 构建新版本
 go build -v -o qq_robot .
