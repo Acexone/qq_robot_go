fd462efaf0b4bbc6104f4137b2eda50017ad6a22
风之凌殇 <fzls.zju@gmail.com>
2021-12-13 20:00:49 +0800

    feat: 调整示例代码

--- qqrobot/qinglong/cmd/main.go
+++ qqrobot/qinglong/cmd/main.go
@@ -9,11 +9,14 @@ import (
 func main() {
 	initLogger()
 
+	logger.Infof("解析cookie")
 	envInfos, err := qinglong.ParseJdCookie()
 	logger.Infof("my: %v %v", envInfos["jd_70a2bcede031c"], err)
 
 	var info *qinglong.JdCookieInfo
 
+	logger.Infof("")
+	logger.Infof("使用不同方式查询cookie信息")
 	for _, param := range []string{"", "jd_70a2bcede031c", "1", "风之凌殇"} {
 		info = qinglong.QueryCookieInfo(param)
 		logger.Infof("%v: %v", param, info)
@@ -21,6 +24,8 @@ func main() {
 
 	chart := qinglong.QueryChartPath(info)
 	summary := qinglong.QuerySummary(info)
-	logger.Info(chart)
-	logger.Info(summary)
+	logger.Infof("")
+	logger.Infof("解析的统计信息如下")
+	logger.Infof("chart: %v", chart)
+	logger.Infof("summary\n%v", summary)
 }
