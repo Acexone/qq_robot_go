481523bd67e370a2819d127806120092cfc53a84
风之凌殇 <fzls.zju@gmail.com>
2021-12-21 15:19:03 +0800

    test: 调整测试程序

--- qqrobot/qinglong/cmd/main.go
+++ qqrobot/qinglong/cmd/main.go
@@ -17,15 +17,17 @@ func main() {
 
 	logger.Infof("")
 	logger.Infof("使用不同方式查询cookie信息")
-	for _, param := range []string{"", "jd_70a2bcede031c", "风之凌殇"} {
+	for _, param := range []string{"", "pin_1", "测试账号-1"} {
 		info = qinglong.QueryCookieInfo(param)
 		logger.Infof("%v: %v", param, info)
 	}
 
 	chart := qinglong.QueryChartPath(info)
 	summary := qinglong.QuerySummary(info)
+	expired := qinglong.QueryCookieExpired(info)
 	logger.Infof("")
 	logger.Infof("解析的统计信息如下")
 	logger.Infof("chart: %v", chart)
-	logger.Infof("summary\n%v", summary)
+	logger.Infof("summary: \n%v\n", summary)
+	logger.Infof("expired: \n%v\n", expired)
 }
