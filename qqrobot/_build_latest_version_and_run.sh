655e14977cf392147843cfa3f20144c9b09bf2da
风之凌殇 <fzls.zju@gmail.com>
2023-06-26 22:03:57 +0800

    feat: 一键运行脚本增加复制配置文件仓库的session文件

--- qqrobot/_build_latest_version_and_run.sh
+++ qqrobot/_build_latest_version_and_run.sh
@@ -8,7 +8,7 @@ go build -v -o qq_robot .
 
 # 拉取私有仓库子模块的最新版本，并将最新配置复制到项目根目录
 git submodule update --recursive --remote
-cp qqrobot/setting/{config.toml,config.yml,device.json} .
+cp qqrobot/setting/{config.toml,config.yml,device.json,session.token} .
 
 # 运行
 ./qq_robot faststart
